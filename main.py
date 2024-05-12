dc66f6ac5d4be80e554e2514f7dd972bc8e8449f
talenelat <20826816+talenelat@users.noreply.github.com>
2024-05-12 18:22:07 +0300

    Print book report

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 def main():
     path_to_book = "./books/frankenstein.txt"
     book_text = get_book_text(path_to_book)
+
     word_count = count_words(book_text)
-    print(word_count)
+    letter_count = count_letter(book_text)
+
+    letter_count_arr = []
+    for letter in letter_count:
+        letter_count_arr.append({"letter": letter, "num": letter_count[letter]})
+    letter_count_arr.sort(reverse=True, key=sort_on)
+
+    print(f"--- Begin report of {path_to_book} ---")
+    print(f"{word_count} words found in the document\n")
+
+    for letter in letter_count_arr:
+        print(f"The '{letter['letter']}' character was found {letter['num']} times")
+
+    print("--- End report ---")
 
 
 def get_book_text(path_to_book):
@@ -15,6 +29,22 @@ def count_words(text):
     words = text.split()
     text_length = len(words)
     return text_length
+
+
+def count_letter(text):
+    letter_count = {}
+    text_lowered = text.lower()
+    for char in text_lowered:
+        if char.isalpha():
+            if char not in letter_count:
+                letter_count[char] = 1
+            else:
+                letter_count[char] += 1
+    return letter_count
+
+
+def sort_on(dict):
+    return dict["num"]
 
 
 main()
