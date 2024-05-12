ca921d7eaab9516a764ded4c9e262c55d49eacc1
talenelat <20826816+talenelat@users.noreply.github.com>
2024-05-12 18:00:20 +0300

    Split getting text content logic

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
+    path_to_book = "./books/frankenstein.txt"
+    book_text = get_book_text(path_to_book)
+    count_words(book_text)
+
+
+def get_book_text(path_to_book):
+    with open(path_to_book) as f:
         file_contents = f.read()
-        count_words(file_contents)
+    return file_contents
+
 
 def count_words(text):
     words = text.split()
     text_length = len(words)
     print(text_length)
+
 
 main()
