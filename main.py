995a457ca6aa1cbaaaf0b1d0d2cd8355c9e97a12
talenelat <20826816+talenelat@users.noreply.github.com>
2024-05-12 17:58:37 +0300

    Count text words

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        count_words(file_contents)
 
+def count_words(text):
+    words = text.split()
+    text_length = len(words)
+    print(text_length)
 
 main()
